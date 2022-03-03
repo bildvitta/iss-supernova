0fdbe1d6f435135f30e422cda2332e9a47445b47
Michael <cocaoaoao@gmail.com>
2022-03-03 14:21:14 -0300

    Observers testing mode fix

--- src/Observers/RealEstateDevelopment/ParameterObserver.php
+++ src/Observers/RealEstateDevelopment/ParameterObserver.php
@@ -3,12 +3,17 @@
 namespace Bildvitta\IssSupernova\Observers\RealEstateDevelopment;
 
 use Bildvitta\IssSupernova\IssSupernova;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 
 class ParameterObserver
 {
     public function created($parameter)
     {
+        if (App::runningUnitTests()) {
+            return;
+        }
+        
         $parameter->loadMissing('realEstateDevelopment');
         $data = $parameter->toArray();
         $data['sync_to'] = 'sys';
@@ -25,6 +30,10 @@ class ParameterObserver
 
     public function updated($parameter)
     {
+        if (App::runningUnitTests()) {
+            return;
+        }
+        
         $parameter->loadMissing('realEstateDevelopment');
         $data = $parameter->toArray();
         $data['sync_to'] = 'sys';
